05af8d1d3e7fa22c981b1bba26e58d3f201dc5b0
Jingjing Fang <59980749+jingjingfang@users.noreply.github.com>
2021-01-19 20:38:13 -0500

    Add files via upload

--- fuzzyartanalysis.sql
+++ fuzzyartanalysis.sql
@@ -2,8 +2,8 @@ USE fuzzyart;
 
 SELECT
 	utm_content, 
-    utm_source, 
-    utm_campaign
+	utm_source, 
+	utm_campaign
 FROM website_sessions
 GROUP BY 1,2,3;
 
@@ -11,7 +11,7 @@ GROUP BY 1,2,3;
 SELECT 
 	utm_source,
 	COUNT(DISTINCT website_session_id) AS sessions, 
-    ROUND(count(*) *100.0/sum(count(*)) OVER(),2) percentage_of_total
+	ROUND(count(*) *100.0/sum(count(*)) OVER(),2) percentage_of_total
 FROM website_sessions
 GROUP BY utm_source
 ORDER BY percentage_of_total DESC;
@@ -20,7 +20,7 @@ ORDER BY percentage_of_total DESC;
 SELECT 
 	utm_content, 
 	COUNT(DISTINCT website_session_id) AS sessions,
-    ROUND(count(*) *100.0/sum(count(*)) OVER (),2) percentage_of_total
+	ROUND(count(*) *100.0/sum(count(*)) OVER (),2) percentage_of_total
 FROM website_sessions
 GROUP BY
 	utm_content    
@@ -30,9 +30,9 @@ ORDER BY percentage_of_total DESC;
 -- display the conversion rate by umt_source
 SELECT 
 	utm_source,
-    COUNT(DISTINCT website_sessions.website_session_id) AS count_of_session,
-    COUNT(DISTINCT orders.order_id) AS count_of_order,
-    COUNT(DISTINCT orders.order_id)/COUNT(DISTINCT website_sessions.website_session_id) AS conv_rt
+	COUNT(DISTINCT website_sessions.website_session_id) AS count_of_session,
+	COUNT(DISTINCT orders.order_id) AS count_of_order,
+	COUNT(DISTINCT orders.order_id)/COUNT(DISTINCT website_sessions.website_session_id) AS conv_rt
 FROM website_sessions
 LEFT JOIN orders
     on orders.website_session_id = website_sessions.website_session_id
@@ -41,8 +41,8 @@ ORDER By conv_rt desc;
     
  -- display the top pages in a descending order
 SELECT
-	 pageview_url,
-     COUNT(DISTINCT website_pageview_id) AS num_of_page_view
+	pageview_url,
+	COUNT(DISTINCT website_pageview_id) AS num_of_page_view
 FROM website_pageviews
 GROUP BY 
 	pageview_url
@@ -52,10 +52,10 @@ ORDER BY
 -- display the montly and weekly session 
 
 SELECT 
-		YEAR(website_sessions.created_at) AS yr,
-		MONTH(website_sessions.created_at) AS mo,
-        min(date(website_sessions.created_at)) as week_start,
-        COUNT(website_sessions.website_session_id) As ct_of_session
+	YEAR(website_sessions.created_at) AS yr,
+	MONTH(website_sessions.created_at) AS mo,
+	min(date(website_sessions.created_at)) as week_start,
+	COUNT(website_sessions.website_session_id) As ct_of_session
 FROM website_sessions
 GROUP BY 1,2, week(created_at)
 ORDER BY week_start;
@@ -64,7 +64,7 @@ ORDER BY week_start;
  SELECT 
 	device_type,
 	count(Distinct website_session_id) AS sessions,
-    ROUND(count(*) *100.0/sum(count(*)) OVER(),2) percentage_of_total
+	ROUND(count(*) *100.0/sum(count(*)) OVER(),2) percentage_of_total
  FROM website_sessions
  GROUP BY device_type; 
 
@@ -72,11 +72,11 @@ ORDER BY week_start;
 -- display the monthly sessions by utm_source for 2019
 SELECT 
 	YEAR(website_sessions.created_at) AS yr,
-    MONTH(website_sessions.created_at) AS mo,
-    COUNT(CASE WHEN utm_source='gsearch' THEN website_sessions.website_session_id ELSE NULL END) as gsearch_paid_session,
-    COUNT(CASE WHEN utm_source='bsearch' THEN website_sessions.website_session_id ELSE NULL END) as bsearch_paid_session,
-    COUNT(CASE WHEN utm_source IS NULL AND http_referer IS NOT NULL THEN website_sessions.website_session_id ELSE NULL END) AS organic_search_session,
-    COUNT(CASE WHEN utm_source IS NULL AND http_referer IS NULL THEN website_sessions.website_session_id ELSE NULL END) AS dire_type_session
+	MONTH(website_sessions.created_at) AS mo,
+	COUNT(CASE WHEN utm_source='gsearch' THEN website_sessions.website_session_id ELSE NULL END) as gsearch_paid_session,
+	COUNT(CASE WHEN utm_source='bsearch' THEN website_sessions.website_session_id ELSE NULL END) as bsearch_paid_session,
+	COUNT(CASE WHEN utm_source IS NULL AND http_referer IS NOT NULL THEN website_sessions.website_session_id ELSE NULL END) AS organic_search_session,
+	COUNT(CASE WHEN utm_source IS NULL AND http_referer IS NULL THEN website_sessions.website_session_id ELSE NULL END) AS dire_type_session
 FROM website_sessions
 LEFT JOIN orders
 	on orders.website_session_id = website_sessions.website_session_id
@@ -87,11 +87,11 @@ GROUP BY 1,2;
 -- display % of total sesions for each utm_source for 2019
 SELECT 
 	YEAR(website_sessions.created_at) AS yr,
-    MONTH(website_sessions.created_at) AS mo,
-    FORMAT(COUNT(CASE WHEN utm_source='gsearch' THEN website_sessions.website_session_id ELSE NULL END) /COUNT( website_sessions.website_session_id *100),2) AS gsearch_of_total,
-    FORMAT(COUNT(CASE WHEN utm_source='bsearch' THEN website_sessions.website_session_id ELSE NULL END) /COUNT( website_sessions.website_session_id *100),2) AS bsearch_of_total,
-    FORMAT(COUNT(CASE WHEN utm_source IS NULL AND http_referer IS NOT NULL THEN website_sessions.website_session_id ELSE NULL END) /COUNT(website_sessions.website_session_id *100),2) AS organi_search_of_total,
-    FORMAT(COUNT(CASE WHEN utm_source IS NULL AND http_referer IS NULL THEN website_sessions.website_session_id ELSE NULL END) /COUNT( website_sessions.website_session_id *100),2) AS dire_search_of_total
+	MONTH(website_sessions.created_at) AS mo,
+	FORMAT(COUNT(CASE WHEN utm_source='gsearch' THEN website_sessions.website_session_id ELSE NULL END) /COUNT( website_sessions.website_session_id *100),2) AS gsearch_of_total,
+	FORMAT(COUNT(CASE WHEN utm_source='bsearch' THEN website_sessions.website_session_id ELSE NULL END) /COUNT( website_sessions.website_session_id *100),2) AS bsearch_of_total,
+	FORMAT(COUNT(CASE WHEN utm_source IS NULL AND http_referer IS NOT NULL THEN website_sessions.website_session_id ELSE NULL END) /COUNT(website_sessions.website_session_id *100),2) AS organi_search_of_total,
+	FORMAT(COUNT(CASE WHEN utm_source IS NULL AND http_referer IS NULL THEN website_sessions.website_session_id ELSE NULL END) /COUNT( website_sessions.website_session_id *100),2) AS dire_search_of_total
 FROM website_sessions
 LEFT JOIN orders
 	on  orders.website_session_id =website_sessions.website_session_id
@@ -101,11 +101,11 @@ GROUP BY 1,2;
   
 -- What is the session to order conversion rate by month?  
 SELECT 
-		YEAR(website_sessions.created_at) as yr,	
-		MONTH(website_sessions.created_at) as mo,
-	    COUNT(DISTINCT website_sessions.website_session_id) As count_of_session,
-        COUNT(DISTINCT orders.order_id) as count_of_order,      
-        FORMAT(COUNT(DISTINCT orders.order_id)/COUNT(DISTINCT website_sessions.website_session_id) *100,2) AS session_conv_rt 
+	YEAR(website_sessions.created_at) as yr,	
+	MONTH(website_sessions.created_at) as mo,
+	COUNT(DISTINCT website_sessions.website_session_id) As count_of_session,
+	COUNT(DISTINCT orders.order_id) as count_of_order,      
+	FORMAT(COUNT(DISTINCT orders.order_id)/COUNT(DISTINCT website_sessions.website_session_id) *100,2) AS session_conv_rt 
 FROM website_sessions
 LEFT JOIN orders
 	on website_sessions.website_session_id = orders.website_session_id
